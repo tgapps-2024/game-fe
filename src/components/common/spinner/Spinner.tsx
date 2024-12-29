d4a7e4b1c114e009ef527b25ac34dd14758573e5
vhoreho <vpubuntu@gmail.com>
2024-12-29 14:15:10 +0300

    Added spinner whena loading component

---
+++ src/components/common/spinner/Spinner.tsx
@@ -0,0 +1,26 @@
+import { FunctionComponent } from "react";
+
+import { cn } from "@/utils/lib/utils";
+
+type Props = {
+  className?: string;
+};
+
+export const Spinner: FunctionComponent<Props> = ({ className }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={cn("animate-spin", className)}
+    >
+      <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+    </svg>
+  );
+};
