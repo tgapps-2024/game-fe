da8de252b9fd8a7800b3ad0e46107a5ecb442cf0
vhoreho <vpubuntu@gmail.com>
2025-01-27 23:50:54 +0300

    added images for modals window

--- src/components/pages/friends/components/invite-modal/cards.data.ts
+++ src/components/pages/friends/components/invite-modal/cards.data.ts
@@ -1,20 +1,28 @@
 import { CardType } from "@/components/common";
+import Coins1 from "@/public/assets/png/friends/buy-friends/coins-1.webp";
+import Coins2 from "@/public/assets/png/friends/buy-friends/coins-2.webp";
+import Coins3 from "@/public/assets/png/friends/buy-friends/coins-3.webp";
+import Coins4 from "@/public/assets/png/friends/buy-friends/coins-4.webp";
+import Coins5 from "@/public/assets/png/friends/buy-friends/coins-5.webp";
+import Coins6 from "@/public/assets/png/friends/buy-friends/coins-6.webp";
 
 export const CARDS = [
-  { buttonText: "Купить", number: 5, badgeValue: 1450 },
-  { buttonText: "Купить", number: 25, badgeValue: 1450 },
-  { buttonText: "Купить", number: 100, badgeValue: 1450 },
-  { buttonText: "Купить", number: 250, badgeValue: 1450 },
+  { buttonText: "Купить", number: 5, badgeValue: 1450, image: Coins1 },
+  { buttonText: "Купить", number: 25, badgeValue: 1450, image: Coins2 },
+  { buttonText: "Купить", number: 100, badgeValue: 1450, image: Coins3 },
+  { buttonText: "-20%", number: 250, badgeValue: 1450, image: Coins4 },
   {
-    buttonText: "Скидка",
+    buttonText: "-20%",
     number: 1000,
     badgeValue: 1450,
     type: CardType.INDIGO,
+    image: Coins5,
   },
   {
-    buttonText: "Скидка",
+    buttonText: "-20%",
     number: 5000,
     badgeValue: 1450,
     type: CardType.ORANGE,
+    image: Coins6,
   },
 ];
