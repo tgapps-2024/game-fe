4799b732a05f99605823bc0e5e51afb4854c003e
vhoreho <vpubuntu@gmail.com>
2025-02-20 21:20:17 +0300

    added offline bonus flow

---
+++ src/services/offline-bonus/queries.ts
@@ -0,0 +1,28 @@
+import Cookies from "js-cookie";
+
+import { AUTH_COOKIE_TOKEN, STALE_TIME } from "@/constants/api";
+import { QueryClient, useMutation, useQuery } from "@tanstack/react-query";
+
+import { invalidateProfileQuery } from "../profile/queries";
+
+import { confirmOfflineBonus, getOfflineBonus } from "./fetcher";
+
+enum QueryKeys {
+  GET_OFFLINE_REWARD = "GET_OFFLINE_REWARD",
+}
+export const useGetOfflineBonus = () =>
+  useQuery({
+    queryKey: [QueryKeys.GET_OFFLINE_REWARD],
+    queryFn: getOfflineBonus,
+    enabled: !!Cookies.get(AUTH_COOKIE_TOKEN),
+    retry: false,
+    staleTime: STALE_TIME,
+  });
+
+export const useConfirmOfflineBonus = (queryClient: QueryClient) =>
+  useMutation({
+    mutationFn: confirmOfflineBonus,
+    onSuccess: () => {
+      invalidateProfileQuery(queryClient);
+    },
+  });
